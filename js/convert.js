2c3e6e17dad4a862d8b476a433c1c8c90c56ec18
conormcstay1 <35769373+conormcstay1@users.noreply.github.com>
2018-04-25 15:00:43 +0100

    currency converter table

--- js/convert.js
+++ js/convert.js
@@ -1,10 +1,42 @@
-function euroToPounds(){
 
-  var amount = parseInt(document.getElementById("value1").value);
-
-  var pounds = amount * 0.86;
-  var message = amount + ' euros converts to ' + pounds + ' sterling.';
-  console.log(message);
-  document.getElementById("resultsentence").innerHTML = message;
-
+//converts euro to pounds
+function euroToPounds() {
+	
+	var amount = parseInt(document.getElementById("value1").value);
+	var pounds = amount * 0.86;
+	  
+	var message = amount + ' euros converts to ' + pounds + ' sterling.';
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+}	
+//converts euro to dollars
+function euroToUSDollars() {
+		
+	var amount = parseInt(document.getElementById("value2").value);
+	var US Dollars = amount * 1.06;
+	 
+	var message = amount + ' euros converts to ' + US dollars + ' dollars.';
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+}	
+//converts euro to yen
+function euroToJapaneseYen() {
+		
+	var amount = parseInt(document.getElementById("value3").value);
+	var Japanese Yen = amount * 121.12;
+	  
+	var message = amount + ' euros converts to ' + Japanese Yen + ' Yen.';
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+} 
+//converts euro to bitcoin
+function euroToBitcoin() {
+		
+	var amount = parseInt(document.getElementById("value4").value);
+	var Bitcoin = amount * 0.00082;
+	  
+	var message = amount + ' euros converts to ' + Bitcoin + ' Bitcoin.';
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+	
 }
